2c957a15557db2b3750317cca5736cb779ff0259
Long <long102220@donga.edu.vn>
2026-02-13 20:54:30 +0700

    first commit

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,108 @@
+CREATE DATABASE usermanage;
+
+-- Kết nối DB (nếu dùng psql)
+-- \c usermanage;
+
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+-- USERS
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    full_name VARCHAR(100),
+    email VARCHAR(100),
+    phone VARCHAR(20),
+
+    user_type VARCHAR(20) NOT NULL,
+
+    last_login_at TIMESTAMP,
+
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP,
+
+    created_by UUID,
+    updated_by UUID,
+
+    deleted_at TIMESTAMP,
+    deleted_by UUID,
+
+    is_active BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+-- ROLES
+CREATE TABLE roles (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+
+    role_code VARCHAR(50) NOT NULL UNIQUE,
+    role_name VARCHAR(100) NOT NULL,
+    description VARCHAR(255),
+
+    is_system BOOLEAN NOT NULL DEFAULT FALSE,
+
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP,
+
+    created_by UUID,
+    updated_by UUID,
+
+    deleted_at TIMESTAMP,
+    deleted_by UUID,
+
+    is_active BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+-- USER_ROLES
+CREATE TABLE user_roles (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+
+    user_id UUID NOT NULL,
+    role_id UUID NOT NULL,
+
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    is_active BOOLEAN NOT NULL DEFAULT TRUE,
+
+    CONSTRAINT fk_user_roles_user
+        FOREIGN KEY (user_id) REFERENCES users(id),
+
+    CONSTRAINT fk_user_roles_role
+        FOREIGN KEY (role_id) REFERENCES roles(id),
+
+    CONSTRAINT uq_user_role UNIQUE (user_id, role_id)
+);
+
+-- PERMISSIONS
+CREATE TABLE permissions (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+
+    permission_code VARCHAR(100) NOT NULL UNIQUE,
+    permission_name VARCHAR(150) NOT NULL,
+    module VARCHAR(100),
+    description VARCHAR(255),
+
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    is_active BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+-- ROLE_PERMISSIONS
+CREATE TABLE role_permissions (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+
+    role_id UUID NOT NULL,
+    permission_id UUID NOT NULL,
+
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    is_active BOOLEAN NOT NULL DEFAULT TRUE,
+
+    CONSTRAINT fk_role_permissions_role
+        FOREIGN KEY (role_id) REFERENCES roles(id),
+
+    CONSTRAINT fk_role_permissions_permission
+        FOREIGN KEY (permission_id) REFERENCES permissions(id),
+
+    CONSTRAINT uq_role_permission UNIQUE (role_id, permission_id)
+);
