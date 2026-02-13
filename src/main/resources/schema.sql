c9d736d66fc35a640e9215288b1bb75984e78a95
Long <long102220@donga.edu.vn>
2026-02-13 21:00:42 +0700

    first commit

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,9 @@
+DROP TABLE IF EXISTS role_permissions CASCADE;
+DROP TABLE IF EXISTS user_roles CASCADE;
+DROP TABLE IF EXISTS permissions CASCADE;
+DROP TABLE IF EXISTS roles CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+
 CREATE DATABASE usermanage;
 
 -- Kết nối DB (nếu dùng psql)
